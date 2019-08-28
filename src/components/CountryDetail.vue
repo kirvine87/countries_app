bc51b57f1f3f4faae242fd9f35e8817daa4d76f9
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-28 17:24:16 +0100

    Adds more displays in country details

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -3,15 +3,29 @@
     <h2>{{country.name}}</h2>
     <p>Capital: {{country.capital}}</p>
     <p>Population: {{country.population}}</p>
+    <h3>Languages:</h3>
+    <p v-for="language of countriesLanguages"> {{language}}</p>
+    <img :src="country.flag"></img>
   </div>
 </template>
 
 <script>
 export default {
   name: 'country-detail',
-  props: ['country']
+  props: ['country'],
+  computed: {
+    countriesLanguages() {
+      return this.country.languages.map((language) => {
+        return language.name
+      })
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+  img {
+    width: 100px;
+    height: 100px;
+  }
 </style>
